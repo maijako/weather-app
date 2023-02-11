b5f48231dd0021a251bbcbb5c89ece32ad8bfbc1
ensignjava <109529105+maijako@users.noreply.github.com>
2023-02-11 11:16:16 +0000

    add function to display the five-day weather forecast

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ $("#search-button").on("click", function(event) {
     var windSpeed = "Wind: "+response.wind.speed+" KPH";
     var humidity = "Humidity "+response.main.humidity+"%";
     today.html(response.name+" ("+date+") "+"</br>"+ celsiusTemp + "</br>"+ windSpeed + "</br>"+ humidity);
-    // displayForecast(response.id);
+    displayForecast(response.id);
   });
   searchHistory.push(city);
   renderButtons();
@@ -50,12 +50,33 @@ function renderButtons() {
      var windSpeed = "Wind: "+response.wind.speed+" KPH";
      var humidity = "Humidity "+response.main.humidity+"%";
      today.html(response.name+" ("+date+") "+"</br>"+ celsiusTemp + "</br>"+ windSpeed + "</br>"+ humidity);
-    //  displayForecast(response.id);
+     displayForecast(response.id);
    });
  });
  
  renderButtons();
 
 // Function to display the five-day weather forecast
-// function displayForecast(cityID) {
-//   var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID
+function displayForecast(cityID) {
+  var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&appid=" + APIKey;
+  $.ajax({
+  url: forecastURL,
+  method: "GET"
+  }).then(function(response) {
+  console.log(response);
+  forecast.empty();
+  for (var i = 0; i < response.list.length; i+=8) {
+  var forecastDate = moment(response.list[i].dt_txt).format("MM/DD/YYYY");
+  var forecastTemp = (response.list[i].main.temp - 273.15).toFixed(2) + "°C";
+  var forecastHumidity = response.list[i].main.humidity + "%";
+  var forecastCard = $("<div>").addClass("card col-md-2 ml-4 bg-primary text-white");
+  var forecastCardBody = $("<div>").addClass("card-body p-2");
+  var forecastTitle = $("<h5>").addClass("card-title").text(forecastDate);
+  var forecastTempP = $("<p>").addClass("card-text").text("Temp: " + forecastTemp);
+  var forecastHumidityP = $("<p>").addClass("card-text").text("Humidity: " + forecastHumidity);
+  forecastCardBody.append(forecastTitle, forecastTempP, forecastHumidityP);
+  forecastCard.append(forecastCardBody);
+  forecast.append(forecastCard);
+  }
+  });
+  }
