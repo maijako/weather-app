4b004f9ab0d4f7576860feda6a9e9e0052544b83
ensignjava <109529105+maijako@users.noreply.github.com>
2023-02-11 13:15:40 +0000

    add generate icon code to city on click function as well

--- assets/script.js
+++ assets/script.js
@@ -19,7 +19,6 @@ $("#search-button").on("click", function(event) {
   }).then(function(response) {
     var weatherIconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
     var iconContainer = $("<div id='weather-icon'>").html("<img src='" + weatherIconUrl + "' alt='Weather Icon'>");
-
     var celsiusTemp = "Temp: "+(response.main.temp - 273.15).toFixed(2)+"°C";
     var windSpeed = "Wind: "+response.wind.speed+" KPH";
     var humidity = "Humidity "+response.main.humidity+"%";
@@ -50,10 +49,13 @@ function renderButtons() {
      method: "GET"
    }).then(function(response) {
      console.log(response);
+     var weatherIconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+     var iconContainer = $("<div id='weather-icon'>").html("<img src='" + weatherIconUrl + "' alt='Weather Icon'>");
      var celsiusTemp = "Temp: "+(response.main.temp - 273.15).toFixed(2)+"°C";
      var windSpeed = "Wind: "+response.wind.speed+" KPH";
      var humidity = "Humidity "+response.main.humidity+"%";
-     today.html(response.name+" ("+date+") "+iconContainer+"</br>"+ celsiusTemp + "</br>"+ windSpeed + "</br>"+ humidity);
+     today.html(response.name+" ("+date+") "+"</br>"+ celsiusTemp + "</br>"+ windSpeed + "</br>"+ humidity);
+     today.append(iconContainer);
      displayForecast(response.id);
    });
  });
