f904cda08dc558a6c3004b7529f7b219f6eb9e87
ensignjava <109529105+maijako@users.noreply.github.com>
2023-02-11 09:38:56 +0000

    added a city variable to an api key variable and moved the ajax call to onclick

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,26 @@
 //Variables
 var APIKey = "54374761b115880386522bedb63f1a22";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=" + APIKey;
-var cities = [];
+var searchHistory = [];
+var city;
 
-// an AJAX call
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-//a console log for grabbing the data 
-console.log(response);
+// This function handles events where the add a city to history when search button is clicked
+$("#search-button").on("click", function(event) {
+  event.preventDefault(); 
+  city = $("#search-input").val().trim();
+  if (!city) {
+    return;
+  }
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  // an AJAX call
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    //a console log for grabbing the data 
+    console.log(response);
+  });
+  searchHistory.push(city);
+  renderButtons();
 });
 
 //Function for displaying city data in search history as buttons
@@ -18,31 +29,20 @@ function renderButtons() {
    $(".list-group").empty();
 
    // Loops through the array of cities from the search history
-   for (var i = 0; i < cities.length; i++) {
+   for (var i = 0; i < searchHistory.length; i++) {
      // Then dynamicaly generates buttons for each city in the array
      var a = $("<button>");
      // Adds a class of movie to our button
      a.addClass("city");
      // Added a data-attribute
-     a.attr("data-name", cities[i]);
+     a.attr("data-name", searchHistory[i]);
      // Provided the initial button text
-     a.text(cities[i]);
+     a.text(searchHistory[i]);
      // Added the button to the buttons-view div
      $(".list-group").append(a);
    }
  }
  
- // This function handles events where the add a city to history when search button is clicked
- $("#search-button").on("click", function(event){
-   event.preventDefault(); 
-   var city = $("#search-input").val().trim();
-   if (!city) {
-     return;
-   }
-   cities.push(city);
-   renderButtons();
- });
-
  // Adding click event listeners to all elements with a class of "city"
  $(document).on("click", ".city", function() {
    // Add logic here to display information about the city
